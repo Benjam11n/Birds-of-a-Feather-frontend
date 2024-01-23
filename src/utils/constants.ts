804ee6bdfc6697bc1f51ca66a16d3b5364e7a98a
Benjamin Wang <ben.wang9000@gmail.com>
2024-01-24 02:38:08 +0800

    added vercel.json

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,2 @@
 export const PAGE_SIZE: number = 10;
-export const BACKEND_URL: string =
-  "https://plankton-app-32jbq.ondigitalocean.app";
+export const BACKEND_URL: string = process.env.BACKEND_URL || "";
