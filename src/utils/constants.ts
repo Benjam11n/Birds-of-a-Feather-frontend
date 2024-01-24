3a7315dd3c7b4e4788aefa49b2dd7cf6d14340ff
Benjamin Wang <ben.wang9000@gmail.com>
2024-01-24 17:04:33 +0800

    more bud fixes

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,3 @@
 export const PAGE_SIZE: number = 10;
-// export const BACKEND_URL: string = import.meta.env.VITE_BACKEND_URL;
-export const BACKEND_URL: string = "http://localhost:8080";
+export const BACKEND_URL: string = import.meta.env.VITE_BACKEND_URL;
+// export const BACKEND_URL: string = "http://localhost:8080";
