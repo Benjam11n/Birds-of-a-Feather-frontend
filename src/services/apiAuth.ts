dbe9ba3794d50a60123d3055a66471cb2199144d
Benjamin Wang <ben.wang9000@gmail.com>
2024-01-24 10:27:33 +0800

    more changes

--- src/services/apiAuth.ts
+++ src/services/apiAuth.ts
@@ -11,6 +11,7 @@ export async function login(user: LoginUser) {
     body: JSON.stringify(user), // send the data as the body
   });
 
+  console.log(BACKEND_URL, response);
   if (!response.ok) throw new Error("Incorrect email or password");
   const { token } = await response.json();
   localStorage.setItem("token", token);
