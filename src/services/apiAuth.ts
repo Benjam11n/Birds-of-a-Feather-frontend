f66f6161e7201ef479333fc12b94b5f2b7629913
Benjamin Wang <ben.wang9000@gmail.com>
2024-01-24 10:58:34 +0800

    update environment variables

--- src/services/apiAuth.ts
+++ src/services/apiAuth.ts
@@ -1,8 +1,9 @@
 import { LoginUser } from "@/types/allTypes";
+import { BACKEND_URL } from "@/utils/constants";
 
 export async function login(user: LoginUser) {
   // make the fetch request with the data as the body
-  const response = await fetch(`${import.meta.env.BACKEND_URL}/login`, {
+  const response = await fetch(`${BACKEND_URL}/login`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -16,7 +17,7 @@ export async function login(user: LoginUser) {
 }
 
 export async function getCurrentUser() {
-  const response = await fetch(`${import.meta.env.BACKEND_URL}/`, {
+  const response = await fetch(`${BACKEND_URL}/`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -44,7 +45,7 @@ export async function signUp({
   });
 
   // make the fetch request with the data as the body
-  const response = await fetch(`${import.meta.env.BACKEND_URL}/signup`, {
+  const response = await fetch(`${BACKEND_URL}/signup`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -56,7 +57,7 @@ export async function signUp({
 }
 
 export async function getUsers() {
-  const response = await fetch(`${import.meta.env.BACKEND_URL}/users`);
+  const response = await fetch(`${BACKEND_URL}/users`);
   if (!response.ok) throw new Error("Failed to fetch users");
 
   const friends = await response.json();
@@ -64,7 +65,7 @@ export async function getUsers() {
 }
 
 export async function logOut(): Promise<void> {
-  const response = await fetch(`${import.meta.env.BACKEND_URL}/logout`, {
+  const response = await fetch(`${BACKEND_URL}/logout`, {
     method: "POST",
     headers: {
       Authorization: `${localStorage.getItem("token")}`,
